bf35438341e71e6e18278b923593dd39a4193e28
Abhishek R <37078073+abhishek14k@users.noreply.github.com>
2023-02-24 22:46:12 +0530

    deployment changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import BarChart from "./charts/BarChart";
+import DoughnutChart from "./charts/DoughnutChart";
+import LineChart from "./charts/LineChart";
+import PieChart from "./charts/PieChart";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Live Crypto Coin Chart</h2>
+      <BarChart />
+      <DoughnutChart />
+      <LineChart />
+      <PieChart />
     </div>
   );
 }
---
+++ src/charts/PieChart.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+import { Chart, registerables } from "chart.js";
+
+Chart.register(...registerables);
+
+const PieChart = () => {
+  const [chart, setChart] = useState([]);
+
+  var baseUrl = "https://api.coinranking.com/v2/coins/?limit=10";
+  var proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  var apiKey = "coinranking87d38360688b1ac777b1a4df10674513e35ed642d494c16c";
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      await fetch(`${proxyUrl}${baseUrl}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": `${apiKey}`,
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+        .then((response) => {
+          response.json().then((json) => {
+            console.log(json);
+            setChart(json.data);
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    fetchCoins();
+  }, []);
+
+  var data = {
+    labels: chart?.coins?.map((x) => x.name),
+    datasets: [
+      {
+        label: "$ Price",
+        // label: `${chart?.coins.length} Coins Available`,
+        data: chart?.coins?.map((x) => x.price),
+        borderWidth: 1,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+      },
+    ],
+  };
+
+  var options = {
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+    legend: {
+      labels: {
+        fontSize: 26,
+      },
+    },
+  };
+
+  return (
+    <div>
+      <Pie data={data} options={options} height={400} />
+    </div>
+  );
+};
+
+export default PieChart;
